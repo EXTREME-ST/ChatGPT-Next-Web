78735aa67f7da266985d04e7bfafd6e9a5072f97
尚书 <119644325+EXTREME-ST@users.noreply.github.com>
2023-05-31 15:47:28 +0800

    Update sidebar.tsx
    update title

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -119,9 +119,9 @@ export function SideBar(props: { className?: string }) {
       }`}
     >
       <div className={styles["sidebar-header"]}>
-        <div className={styles["sidebar-title"]}>ChatGPT Next</div>
+        <div className={styles["sidebar-title"]}>ChatGPT|EXTREME</div>
         <div className={styles["sidebar-sub-title"]}>
-          Build your own AI assistant.
+          博客：www.extremest.top
         </div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
           <ChatGptIcon />
